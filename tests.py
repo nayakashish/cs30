93b412c358e1f9897245c8127c34f1086a2333c4
nayakashish <a.samuel.nayak@gmail.com>
2021-09-16 12:33:27 -0700

    pycharm array of grid color coord

--- tests.py
+++ tests.py
@@ -2,22 +2,58 @@ import pyautogui
 import time
 
 
-def screenshot(left, top, width, height):
-    im1 = pyautogui.screenshot('testimage.png', region=(left, top, width, height))
-    return im1
+def screenshot(left, top, width, height, title=''):
+    if title == '':
+        image = pyautogui.screenshot(region=(left, top, width, height))
+    else:
+        image = pyautogui.screenshot(title, region=(left, top, width, height))
+    return image
+
 
 # GRID is 45x45? px
 
-# pos = [i for i in pyautogui.locateOnScreen('easy_grid.png')]
-# print(pos)
-
-print('Yes')
-# time.sleep(3)
-
-# screenshot(pos[0], pos[1], pos[2], pos[3])
-# left 1162 top 520 45 45
-d = screenshot(1387, 430, 45, 45)
-print(d.getpixel((23, 22.5)))
+# pos = [i for i in pyautogui.locateOnScreen('easy_grid.png')]  # Must be new board
+# pos = [left, top, width, height]
+pos = [1027, 385, 448, 359] # School Computer
+# pos = [1207, 421, 450, 360]  # Home Computer
 
 
-# Red 211 47 47
+left = 23
+top = 22.5
+shot = screenshot(pos[0], pos[1], pos[2], pos[3])
+lst = []
+templst = []
+print(lst)
+for j in range(8):
+
+    for i in range(10):
+
+        # screenshot(left, top, 45, 45, '{}.png'.format(num))
+        templst.append(shot.getpixel((left, top)))
+
+
+        left += 45
+    lst.append(templst)
+    templst = []
+    top += 45
+    left = 23
+
+print(lst)
+''' 
+Colour Codes RGB
+Three 211 47 47
+Two 118 161 96
+One 25 118 210
+Four
+Five
+Six
+Seven
+Eight
+
+
+Bkg1 170 215 81
+Bkg2 162 209 73
+
+Empty1 229 194 159
+Empty2 215 184 153
+'''
